c57af7fee069a209909e10d197c7a863f49421b4
addumairi <azharaddumairi@gmail.com>
2022-09-02 03:10:00 +0800

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,42 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 function Home() {
+  const contactList = [
+    {
+      id: 1,
+      icon: FaLinkedinIn,
+      link: "https://www.linkedin.com/in/addumairi-azhar-maksum-206bb2157/",
+    },
+    {
+      id: 2,
+      icon: FaGithub,
+      link: "https://github.com/Addumairi99",
+    },
+    {
+      id: 3,
+      icon: AiOutlineMail,
+      link: "mailto:azharaddumairi@gmail.com",
+    },
+    {
+      id: 4,
+      icon: BsFillPersonLinesFill,
+      link: "https://wa.me/6282215444845",
+    },
+  ];
+
+  const renderContact = () => {
+    return contactList.map((val) => {
+      return (
+        <div key={val.id}>
+          <a href={val.link} target="_blank" rel="noreferrer">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 bg-white">
+              <val.icon />
+            </div>
+          </a>
+        </div>
+      );
+    });
+  };
   return (
     <div name="home" className="w-full h-screen text-center bg-gray-900 ">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
@@ -25,42 +61,7 @@ function Home() {
             developing web applications.
           </p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-            <a
-              href="https://www.linkedin.com/in/addumairi-azhar-maksum-206bb2157/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 bg-white">
-                <FaLinkedinIn />
-              </div>
-            </a>
-            <a
-              href="https://github.com/Addumairi99"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 bg-white">
-                <FaGithub />
-              </div>
-            </a>
-            <a
-              href="mailto:azharaddumairi@gmail.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 bg-white">
-                <AiOutlineMail />
-              </div>
-            </a>
-            <a
-              href="https://wa.me/6282215444845"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 bg-white">
-                <BsFillPersonLinesFill />
-              </div>
-            </a>
+            {renderContact()}
           </div>
         </div>
       </div>
